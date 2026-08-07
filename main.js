a53f1f441d4948109edb51a09663090029a9b50b
Martin Melendez <martinf.melendez@gmail.com>
2026-08-07 13:47:48 -0300

    Verificar error al reiniciar el juego

--- main.js
+++ main.js
@@ -1,37 +1,45 @@
-/*Instrucciones del Checkpoint
-1. Estructura de Decisión (Condicionales)
+/*
 
-Debes usar if, else if y else para bifurcar el flujo. Por ejemplo: validar si un número es mayor que otro, si un string coincide con una clave, o si un usuario tiene permisos. Opcional: Puedes usar un switch si tienes múltiples opciones fijas (como un menú de selección).
-2. Estructura de Repetición (Bucles)
+Entregable
+El Escenario
 
-Debes implementar al menos un bucle (for, while o do...while).
+Debes continuar el desarrollo de tu simulador, implementando el uso de funciones para mejorar y modularizar procesos en tu script para simplificar la lógica y la reutilización de fragmentos de código sin la necesidad de repetirlos.
+Criterios de Aceptación
 
-    Usa for si conoces la cantidad de veces que quieres repetir algo (ej: mostrar los números del 1 al 10).
-    Usa while si la repetición depende de una condición que el usuario puede cambiar (ej: repetir hasta que el usuario escriba "ESC").
+El repositorio debe:
+
+    Crea al menos 3 funciones siguiendo el algoritmo básico de todo programa (entradas de datos, procesamiento de datos y mostrar los resultado de salida de los mismos.)
+    Uso de parámetros: Ingresar información para procesarla dentro de la funcion mediante el uso de parámetros y argumentos en todas las funciones.
+    Uso de return: Al menos una funcion debe retornar un valor para utilizarse en el algoritmo.
+    Función flecha: Al menos una funcion debe ser del tipo flecha, optimizando un proceso simple.
+    Realizar llamadas(invocar) a las funciones que utilizaste en tu simulador
+    Agrega los ciclos de iteración y condicionales necesarios, para que tu proyecto funcione correctamente.
+    La interacción del simulador se hará íntegramente sobre la Consola y Alert, con los datos previamente solicitados mediante prompt.
+
+Método de entrega
+
+Sube tus archivos a un repositorio de GitHub y comparte la URL. Asegúrate de que el repositorio sea público para que pueda ser revisado.
+
 */
 
-//Creacion del juego adivina el numero
+//Creacion del juego adivina el numero utilizando funciones
 
 let intentos = 0;
-
+const maxIntentos = 3;
 let numeroSecreto = Math.floor(Math.random() * 10) + 1; //Creacion numero aleatorio entre 1 y 10
 console.log(numeroSecreto);
 
-alert(
-  "Bienvenido al juego de adivina el numero, tienes 3 intentos para adivinar el numero secreto entre 1 y 10",
-);
-
 function validarNumeroIngresado(num1) {
   if (num1 < 0 || num1 > 10 || isNaN(num1)) {
-    alert("El numero ingresado no es valido");
     return false;
   }
   return true;
 }
 
-const juegoTerminado = (intentos) => {
-  if (intentos === 3) {
+const juegoTerminado = (intentos, numeroSecreto) => {
+  if (intentos === maxIntentos) {
     alert(`Game Over! El numero era ${numeroSecreto}`);
+    reiniciarJuego();
   }
 };
 
@@ -44,29 +52,34 @@ const reiniciarJuego = () => {
 function volverAJugar() {
   const continuar = confirm("Quieres jugar otra vez?");
   if (continuar) {
-    reiniciarJuego();
     return true;
   }
   return false;
 }
 
-while (intentos < 3) {
+while (intentos < maxIntentos) {
   const numeroUsuario = parseInt(prompt("Adivina el numero entre 1 y 10"));
 
   if (validarNumeroIngresado(numeroUsuario)) {
     intentos++;
     console.log(`Intento numero: ${intentos}`);
-
-    if (numeroUsuario === numeroSecreto) {
-      alert("Felicidades! Adivinaste el numero");
-      if (!volverAJugar()) {
-        break;
-      }
-    } else if (numeroUsuario < numeroSecreto) {
-      alert("El numero ingresado es menor al numero secreto");
-    } else {
-      alert("El numero ingresado es mayor al numero secreto");
-    }
+  } else {
+    alert("El numero ingresado no es valido");
+    continue;
   }
-  juegoTerminado(intentos);
+
+  if (numeroUsuario === numeroSecreto) {
+    alert("Felicidades! Adivinaste el numero");
+    if (!volverAJugar()) {
+      break;
+    } else {
+      reiniciarJuego();
+    }
+  } else if (numeroUsuario < numeroSecreto) {
+    alert("El numero ingresado es menor al numero secreto");
+  } else {
+    alert("El numero ingresado es mayor al numero secreto");
+  }
+
+  juegoTerminado(intentos, numeroSecreto);
 }
