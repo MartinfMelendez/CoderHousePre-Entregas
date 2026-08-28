31322679a77d9a55e04bcea1d3672107014ba965
Martin Melendez <martinf.melendez@gmail.com>
2026-08-27 22:35:57 -0300

    se agrega objetivos de la entrega

---
+++ main.js
@@ -0,0 +1,26 @@
+/*Entregable
+Instrucciones paso a paso:
+
+    Creación de la Estructura:
+        Define una Clase llamada según tu proyecto (ejemplo: Producto, Item, Usuario).
+        El constructor debe recibir al menos 4 parámetros para inicializar propiedades (ej: nombre, precio, categoria, stock).
+    Añadir Comportamiento:
+        Crea un método dentro de la clase que realice una operación lógica.
+        Ejemplo: un método vender(cantidad) que reste el número del stock o un método aplicarDescuento(porcentaje) que modifique el precio.
+    Instanciación:
+        Crea al menos tres objetos diferentes usando la palabra clave new y guárdalos en constantes.
+    Verificación:
+        Ejecuta los métodos de tus objetos creados y usa console.log para mostrar los resultados finales de las propiedades modificadas.
+    Contexto:
+        El script debe continuar con la logica y herramientas (ciclos, funciones, etc.) del simulador elegido, siendo la Class y los objetos parte del mismo y no una aplicación aislada
+
+Pasos sugeridos
+
+    Define tu entidad: Piensa en qué vas a gestionar. Si es una tienda, tu entidad es  Producto . Si es un gimnasio, es  Socio .
+    Escribe la clase: Define el  constructor  con los parámetros necesarios.
+    Agrega lógica: Crea un método que haga un cálculo. Por ejemplo, si es un producto, un método  sumarIva()  que multiplique el precio por 1.21.
+    Prueba tu código: Crea variables para tus objetos y usa  console.log  para ver si los métodos funcionan como esperas.
+
+Entrega:
+
+Sube tus archivos a un repositorio de GitHub y comparte la URL. Asegúrate de que el repositorio sea público para que pueda ser revisado.*/
